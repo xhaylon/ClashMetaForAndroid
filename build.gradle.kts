a4d77470785c1f2429d745707de0b6bf197bcb86
cubemaze <maze.y2b@gmail.com>
2023-06-03 22:49:49 +0800

    chore: bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,8 +40,8 @@ subprojects {
             minSdk = 21
             targetSdk = 31
 
-            versionName = "2.8.4"
-            versionCode = 208004
+            versionName = "2.8.5"
+            versionCode = 208005
 
             resValue("string", "release_name", "v$versionName")
             resValue("integer", "release_code", "$versionCode")

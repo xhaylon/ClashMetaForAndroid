927727dee13b7e116e8ccee12689565a076134e0
wwqgtxx <wwqgtxx@gmail.com>
2023-10-22 20:31:24 +0800

    dependent clean

--- core/src/main/golang/native/tun/metadata_open.go
+++ core/src/main/golang/native/tun/metadata_open.go
@@ -3,17 +3,17 @@
 package tun
 
 import (
-	C "github.com/Dreamacro/clash/constant"
-	"github.com/sagernet/sing/common/metadata"
 	"net"
+
+	C "github.com/Dreamacro/clash/constant"
 )
 
 func createMetadata(lAddr, rAddr *net.TCPAddr) *C.Metadata {
 	return &C.Metadata{
 		NetWork:    C.TCP,
 		Type:       C.SOCKS5,
-		SrcIP:      metadata.SocksaddrFromNet(lAddr).Addr,
-		DstIP:      metadata.SocksaddrFromNet(rAddr).Addr,
+		SrcIP:      lAddr.AddrPort().Addr(),
+		DstIP:      rAddr.AddrPort().Addr(),
 		SrcPort:    uint16(lAddr.Port),
 		DstPort:    uint16(rAddr.Port),
 		Host:       "",

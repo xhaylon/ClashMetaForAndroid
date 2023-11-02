f8a6a6afe284eb8a9127235a4c236a116d083ec9
Steve Johnson <stevejohnson1438@proton.me>
2023-11-02 19:17:58 +0800

    chore: update version

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,8 +40,8 @@ subprojects {
             minSdk = 21
             targetSdk = 31
 
-            versionName = "2.8.10"
-            versionCode = 208010
+            versionName = "2.9.0"
+            versionCode = 209000
 
             resValue("string", "release_name", "v$versionName")
             resValue("integer", "release_code", "$versionCode")
